75286adf0bf322893a216c1dd5ff081033059763
Steve Norum <stevenorum@gmail.com>
2018-04-19 16:32:36 -0400

    Restructuring setup.py to make it easier to script pypi deployments.

--- setup.py
+++ setup.py
@@ -4,19 +4,23 @@ from distutils.core import setup
 
 MAJOR_VERSION='0'
 MINOR_VERSION='0'
-PATCH_VERSION='2'
+PATCH_VERSION='3'
 
 VERSION = "{}.{}.{}".format(MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION)
 
-setup(
-    name = 'toco',
-    packages = ['toco'],
-    version = VERSION,
-    description = 'Basic tools for interacting with DynamoDB.',
-    author = 'Steve Norum',
-    author_email = 'sn@drunkenrobotlabs.org',
-    url = 'https://github.com/stevenorum/toco',
-    download_url = 'https://github.com/stevenorum/toco/archive/{}.tar.gz'.format(VERSION),
-    keywords = ['python','aws','dynamodb'],
-    classifiers = [],
-)
+def main():
+    setup(
+        name = 'toco',
+        packages = ['toco'],
+        version = VERSION,
+        description = 'Basic tools for interacting with DynamoDB.',
+        author = 'Steve Norum',
+        author_email = 'sn@drunkenrobotlabs.org',
+        url = 'https://github.com/stevenorum/toco',
+        download_url = 'https://github.com/stevenorum/toco/archive/{}.tar.gz'.format(VERSION),
+        keywords = ['python','aws','dynamodb'],
+        classifiers = [],
+    )
+
+if __name__ == "__main__":
+    main()
